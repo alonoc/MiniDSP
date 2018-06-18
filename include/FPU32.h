683ab53a35a556c55645dea02c866a2170673fce
Alonso Loaiza <aloaizapereira@gmail.com>
2018-06-18 09:58:48 -0600

    Added initial interface of a floating point unit of 32 units

---
+++ include/FPU32.h
@@ -0,0 +1,38 @@
+#ifndef FPU32_H_
+#define FPU32_H_
+
+#include "systemc.h"
+
+SC_MODULE(FloatingAdder32)
+{
+	sc_in< sc_uint<32> > OpA;
+	sc_in< sc_uint<32> > OpB;
+	sc_out< sc_uint<32> > Result;
+
+	void doIt( void );
+
+	SC_CTOR(FloatingAdder32)
+	{
+		SC_METHOD( doIt );
+			sensitive << OpA;
+			sensitive << OpB;
+	}
+};
+
+SC_MODULE(FloatingMultiplier32)
+{
+	sc_in< sc_uint<32> > OpA;
+	sc_in< sc_uint<32> > OpB;
+	sc_out< sc_uint<32> > Result;
+
+	void doIt( void );
+
+	SC_CTOR(FloatingMultiplier32)
+	{
+		SC_METHOD( doIt );
+			sensitive << OpA;
+			sensitive << OpB;
+	}
+};
+
+#endif
---
+++ src/FPU32.cpp
@@ -0,0 +1,11 @@
+#include "FPU32.h"
+
+void FloatingAdder32::doIt( void )
+{
+
+}
+
+void FloatingMultiplier32::doIt( void )
+{
+	
+}
