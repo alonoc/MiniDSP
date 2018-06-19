f553b740920e44ce97ce0f1960b46b898749cd33
luigi1801 <luigianargo@gmail.com>
2018-06-18 23:46:12 -0600

    Agrego modulo IDFT y me cague en el main (culpa de Edward)

---
+++ include/IDFT.h
@@ -0,0 +1,41 @@
+#ifndef IDFT_H_
+#define IDFT_H_
+
+#include "systemc.h"
+
+SC_MODULE(IDFT)
+{
+	sc_in_clk clock;
+	sc_in<bool> f_Enable;
+	sc_in<bool> reset;
+	sc_in< int > N;
+	sc_in< double > InReal;
+	sc_in< double > InImaginary;
+	sc_out< double > OutReal;
+	sc_out< double > OutImaginary;
+
+	double *Samples;
+	int _N;
+	int gnral_counter;
+	bool readingSamples = 0;
+	bool calculating = 0;
+
+	void doIt( void );
+
+	SC_CTOR(IDFT)
+	{
+		SC_METHOD( doIt );
+			sensitive << clock.pos();
+			sensitive << reset;
+	}
+
+	~IDFT()
+	{
+		if(Samples){
+			delete[] Samples;
+		}
+	}
+};
+
+
+#endif
---
+++ src/IDFT.cpp
@@ -0,0 +1,75 @@
+#include "IDFT.h"
+#include <math.h>
+
+//PLEASE DELETE THIS INCLUDE
+#include <iostream>
+
+void IDFT::doIt( void )
+{
+	if(reset == 1){
+		//std::cout << "Reseting...." << std::endl << std::endl;
+		_N = 0;
+		gnral_counter = 0;
+		readingSamples = 0;
+		calculating = 0;
+	}
+	else if(readingSamples == 1)
+	{
+		//std::cout << "ReadingSamples...." << std::endl;
+		Samples[gnral_counter] = InReal;
+		Samples[gnral_counter+_N] = InImaginary;
+		gnral_counter++;
+		//std::cout << "Sample read: " << gnral_counter << std::endl;
+
+		if(gnral_counter == _N)
+		{
+			readingSamples = 0;
+			calculating = 1;
+			gnral_counter = 0;
+			std::cout << std::endl;
+		}
+	}
+	else if(calculating == 1)
+	{
+		//std::cout << "Calcuting IRDFT...." << std::endl;
+
+		sc_uint<16> n, k;
+		double result1, result2;
+	
+		result1 = 0;
+		result2 = 0;
+		for(n = 0; n < _N; n++)
+		{	
+			double mult1 = Samples[n]*cos(2.0*M_PI*gnral_counter*n/_N);
+			double mult2 = Samples[n+_N]*sin(2.0*M_PI*gnral_counter*n/_N);
+			result1 += mult1;
+			result1 -= mult2;
+
+			mult1 = Samples[n]*sin(2.0*M_PI*gnral_counter*n/_N);
+			mult2 = Samples[n+_N]*cos(2.0*M_PI*gnral_counter*n/_N);
+			result1 += mult1;
+			result1 += mult2;
+		}
+		OutReal = result1/_N;
+		OutImaginary = result2/_N;
+
+		gnral_counter++;
+		//std::cout << "Result Calculated: " << gnral_counter << std::endl;
+		if(gnral_counter == _N)
+		{
+			readingSamples = 0;
+			calculating = 0;
+			gnral_counter = 0;
+		}
+		
+	}
+	else if(f_Enable == 1){
+		//std::cout << "Enabling...." << std::endl << std::endl;
+		Samples = (double*) calloc(N*2, sizeof(double));
+		_N = N;
+		gnral_counter = 0;
+		readingSamples = 1;
+		calculating = 0;
+	}	
+}
+
