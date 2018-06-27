6a4ca7c87afa949958926fd85877fb3582e5fa9f
Guillermo Rodriguez <roguille@gmail.com>
2018-06-27 08:55:12 -0600

    Create ALU.cpp

---
+++ src/ALU.cpp
@@ -0,0 +1,46 @@
+#include "ALU.h"
+
+void ALU::operate()
+{
+    {
+        ZERO = false;
+        data1 = OPa.read();
+        data2 = OPb.read();
+        
+        switch(OP.read())
+        {
+                
+            case 0:
+                result = data1 + data2;
+                break;
+                
+            case 1:
+                result = data1 - data2;
+                break;
+                
+            case 2:
+                result = data1 + 1;
+                break;
+                
+            case 3:
+                result = data1 - 1;
+                break;
+                
+            case 4:
+                result = data1 && data2;
+                break;
+                
+            case 5:
+                result = data1 || data2;
+                break;
+                
+                            
+        }
+        
+        RESULT.write(result.range(3, 0));
+        
+              
+        if(result == 0)
+            ZERO.write(true);
+    }
+}
